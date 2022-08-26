a61115aa17c69370b58a7495a963d7e0e128b960
@cristodca (Cristopher D. Chavez) <cristodca.studio@gmail.com>
2022-08-26 13:01:15 -0500

    google tag manager script

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,15 +11,35 @@ const Document = () => {
         <Script
           id="google-tag-url"
           async
+          strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-ZX957538N8"
         ></Script>
-        <Script id="google-tag-script" strategy="afterInteractive" dangerouslySetInnerHTML={{ _html: `window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag(&lsquo;js&lsquo;, new Date()); gtag(&lsquo;config&lsquo;,
-          &lsquo;G-ZX957538N8&lsquo;);`}}>
-        </Script>
+        <Script
+          id="google-tag-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}gtag(&lsquo;js&lsquo;, new Date()); gtag(&lsquo;config&lsquo;,&lsquo;G-ZX957538N8&lsquo;);`,
+          }}
+        ></Script>
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5Z5NJ48');`,
+          }}
+        ></Script>
+        {/* <!-- End Google Tag Manager --> */}
       </Head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        {/* <noscript
+          dangerouslySetInnerHTML={{
+            _html: `<iframesrc="https://www.googletagmanager.com/ns.html?id=GTM-5Z5NJ48"height="0"width="0"style="display:none;visibility:hidden"></iframesrc=>`,
+          }}
+        ></noscript> */}
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
         <Main />
       </body>
     </Html>
