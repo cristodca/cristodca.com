0a478cd08c8b46a44f639c4a1c9f8e5dbe4eac50
@cristodca (Cristopher D. Chavez) <cristodca.studio@gmail.com>
2022-08-26 15:01:05 -0500

    hotjar script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,8 +16,19 @@ function MyApp({ Component, pageProps }: AppProps) {
         {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}gtag('js', new Date()); gtag('config','G-ZX957538N8');`}
       </Script>
       {/* <!-- Google Tag Manager --> */}
-      <Script id="google-tag-manager" strategy="afterInteractive">
+      <Script id="google-tag-manager" strategy="lazyOnload">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5Z5NJ48');`}
+      </Script>
+      {/* <!-- Hotjar Tracking Code for https://www.cristodca.com --> */}
+      <Script id="hotjar-script" strategy="lazyOnload">
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3127211,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
       </Script>
 
       <Component {...pageProps} />
